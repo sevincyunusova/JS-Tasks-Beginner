517c65b8704dbfc3e4f3bb822803c8f023408e70
sevincyunusova <yunusovasevinc08@gmail.com>
2026-08-06 00:54:19 +0400

    complete search

--- Task2-Search/main.js
+++ Task2-Search/main.js
@@ -1,1 +1,15 @@
-const input = document
+const searchInput = document.querySelector('.search-input');
+const products = document.querySelectorAll('.product');
+
+searchInput.addEventListener('input', () => {
+    const searchValue = searchInput.value.toLowerCase();
+    products.forEach(product => {
+        const productText = product.textContent.toLowerCase();
+        if (productText.includes(searchValue)) {
+            product.classList.remove('hidden');
+        }
+        else {
+            product.classList.add('hidden');
+        }
+    });
+});
